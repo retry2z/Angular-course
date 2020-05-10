eea55ee4d2530b6fb900815da5248800a2acf591
Hristiyan Hristov <32390803+retry2z@users.noreply.github.com>
2020-05-10 08:53:57 +0300

    list

--- material-demo/src/app/app.module.ts
+++ material-demo/src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenusComponent } from './menus/menus.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ListsComponent } from './lists/lists.component';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { GridsComponent } from './grids/grids.component';
+import { MatGridListModule} from '@angular/material/grid-list';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenusComponent,
+    ListsComponent,
+    GridsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
